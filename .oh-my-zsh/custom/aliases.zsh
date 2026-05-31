0a7874df3fb3c08bd0c828f4007465a18d5d50ee
janis-h <janis_h.aguilar@yahoo.com>
2026-05-31 09:26:02 -0700

    zshrc: add color to man and help pages

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -28,17 +28,25 @@ alias cdot='cd "$DOTFILES"'
 export DOT_CONFIG="$HOME/dotfiles/.config"
 export I3_CONFIG="$DOT_CONFIG/i3/config"
 export NVIM_CONFIG="$DOT_CONFIG/nvim/init.vim"
+export ZSH_CONFIG="$DOT_CONFIG/.zshrc"
 export ZSH_ALIASES_CONFIG="$ZSH_CUSTOM/aliases.zsh"
 
 # nvim config edits
 alias ni3config='nvim "$I3_CONFIG"'
 alias nvimrc='nvim "$NVIM_CONFIG"'
+alias nzsh='nvim "$ZSH_CONFIG"'
 alias nzsha='nvim "$ZSH_ALIASES_CONFIG"'
 
-# source configs
-alias si3config='source "$I3_CONFIG"'
-alias svimcrc='source "$NVIM_CONFIG"'
-alias szshaliases='source "ZSH_ALIASES_CONFIG"'
+# program shortcuts
+alias bat=batcat 
+
+# colorize man pages with batcat
+export MANPAGER='sh -c "col -bx | batcat -l man -p"'
+export MANROFFOPT="-c"
+
+# colorize --help pages with batcat
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
 
 # PATH
 export PATH="$PATH:$SCRIPT_BIN"
