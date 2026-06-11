ffffc3a2c94a1b902bd8a2a8c385dc6663a1c1cc
janis-h <janis_h.aguilar@yahoo.com>
2026-06-11 15:48:08 -0700

    nvim: add description to opts

--- modules/nvim/.config/nvim/init.lua
+++ modules/nvim/.config/nvim/init.lua
@@ -85,82 +85,88 @@ Plug('tpope/vim-surround')
 -- end Plug plugin declaration
 vim.call('plug#end')
 
--- Plugin setup
+-- Plugin setup:
 require("plugins.grapple")
 require("plugins.telescope")
-
--- TODO: setup harpoon
---           reference link: https://deepwiki.com/ThePrimeagen/harpoon/1.1-installation-and-setup
 
 -- Color Scheme: 
 vim.cmd.colorscheme("gruvbox")
 
 -- Basic remaps:
-local opts = { noremap = true }
+local function opts(desc)
+    return {
+        noremap = true,
+        silent = true,
+        desc = desc,
+    }
+end
 
 -- leader key
 vim.g.mapleader = " " -- Space as the leader key
 
--- write to file
-vim.keymap.set('n', '<leader><leader>', ':w<CR>', opts)
--- source vim file
-vim.keymap.set('n', '<leader><CR>', ':so ~/.config/nvim/init.lua<CR>', opts)
--- vertical explorer
-vim.keymap.set('n', '<leader>v', ':Vex<CR>', opts)
--- horizontal explorer  
-vim.keymap.set('n', '<leader>s', ':Sex<CR>', opts)
--- next in quickfix list
-vim.keymap.set('n', '<M-j>', ':cnext<CR>', opts)
--- prev in quickfix list
-vim.keymap.set('n', '<M-k>', ':cprev<CR>', opts)
--- clear highlighted search
-vim.keymap.set('n', '<leader>cl', ':nohls<CR>', opts)
--- create new line and paste copied text 
-vim.keymap.set('n', '<leader>p', 'o<ESC>Pk<CR>', opts)
--- yank into system clipboard (need to use motions afterwards)
-vim.keymap.set('n', '<leader>y', '"+y', opts)
--- yank entire file into system clipboard
-vim.keymap.set('n', '<leader>Y', 'gg"+yG', opts)
--- delete current selection, yank to void register (to avoid replacing prev yank)
-vim.keymap.set('v', '<leader>p', '"_dp', opts)
--- copy into system clipboard
-vim.keymap.set('v', '<leader>y', '"+y', opts)
--- move line up
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
--- move line down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
+-- Basic Remaps:
+vim.keymap.set('n', '<leader><leader>', ':w<CR>', opts('Write to file'))
+vim.keymap.set('n', '<leader><CR>', ':so ~/.config/nvim/init.lua<CR>', opts('Source init.lua'))
+vim.keymap.set('n', '<leader>v', ':Vex<CR>', opts('Vertical explorer'))
+vim.keymap.set('n', '<leader>s', ':Sex<CR>', opts('Horizontal explorer'))
+vim.keymap.set('n', '<M-j>', ':cnext<CR>', opts('Next quickfix item'))
+vim.keymap.set('n', '<M-k>', ':cprev<CR>', opts('Prev quickfix item'))
+vim.keymap.set("n", "<leader>co", "<cmd>copen<cr>", opts("Open quickfix"))
+vim.keymap.set("n", "<leader>cn", "<cmd>cnext<cr>", opts("Next quickfix item"))
+vim.keymap.set("n", "<leader>cp", "<cmd>cprev<cr>", opts("Previous quickfix item"))
+vim.keymap.set("n", "<leader>cq", "<cmd>cexpr []<cr><cmd>cclose<cr>", opts("Clear quickfix"))
+vim.keymap.set('n', '<leader>cl', ':nohls<CR>', opts('Clear highlighted search'))
+vim.keymap.set("v", "<leader>d", '"_d', opts("Delete selection without yanking it"))
+vim.keymap.set('v', '<leader>p', '"_dp', opts('Delete current selection, yank into void register (to avoid replacing prev yank)'))
+vim.keymap.set('n', '<leader>P', 'o<ESC>Pk<CR>', opts('Create new line and paste copied text'))
+vim.keymap.set('n', '<leader>y', '"+y', opts('Yank into system clipboard'))
+vim.keymap.set('n', '<leader>Y', 'gg"+yG', opts('Yank entire file into system clipboard'))
+vim.keymap.set('v', '<leader>y', '"+y', opts('Copy into system keyboard'))
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts('Move line up'))
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts('Move line down'))
 
 -- Plugin: Telescope remaps
--- telescope plugin find_files - list files in current directory
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', opts)
--- telescope git files - fuzzy seach git ls-files
-vim.keymap.set('n', '<leader>gg', ':Telescope git_files<CR>', opts)
--- telescope plugin live_grep - search for string in current directory
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', opts)
--- telescope plugin oldfiles - lists previously open files
-vim.keymap.set('n', '<leader>fo', ':Telescope oldfiles<CR>', opts)
--- telescope plugin buffers - lists open buffers in current neovim instance
-vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', opts)
--- telescope plugin help_tags - lists available help tags
-vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', opts)
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', opts('Find files'))
+vim.keymap.set('n', '<leader>gg', ':Telescope git_files<CR>', opts('Find git files'))
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', opts('Search Text'))
+vim.keymap.set('n', '<leader>fo', ':Telescope oldfiles<CR>', opts('Find recent files'))
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', opts('Find buffers'))
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', opts('Find help tags'))
 
 -- Plugin: Fugitive remaps
--- git quick type
-vim.keymap.set('n', '<leader>gi', ':Git ' , opts)
--- git add
-vim.keymap.set('n', '<leader>ga', ':Git add .<CR>'  , opts)
--- git commit -m
-vim.keymap.set('n', '<leader>gc', ':Git co "', opts)
-
+vim.keymap.set('n', '<leader>g:', ':G' , opts('Fugitive Git command'))
+vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", opts("Git status"))
+vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", opts("Git blame"))
+vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", opts("Git diff"))
 
 -- Plugin: Lazygit remaps
-vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', opts)
+vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', opts('Open lazygit'))
 
 -- tmux-sessionizer
 -- Ctrl-f opens the project picker
 -- Alt bindings run TS_SESSION_COMMANDS by index in the active project session.
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<M-1>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
-vim.keymap.set("n", "<M-2>", "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>")
-vim.keymap.set("n", "<M-3>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
-vim.keymap.set("n", "<M-4>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", opts('Opens tmux-sessionizer project picker'))
+vim.keymap.set(
+    "n", 
+    "<M-1>", 
+    "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>",
+    opts('Runs TS_SESSION_COMMANDS[0]')
+)
+vim.keymap.set(
+    "n", 
+    "<M-2>", 
+    "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>",
+    opts('Runs TS_SESSION_COMMANDS[1]')
+)
+vim.keymap.set(
+    "n", 
+    "<M-3>", 
+    "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>",
+    opts('Runs TS_SESSION_COMMANDS[2]')
+)
+vim.keymap.set(
+    "n", 
+    "<M-4>", 
+    "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>",
+    opts('Runs TS_SESSION_COMMANDS[3]')
+)
