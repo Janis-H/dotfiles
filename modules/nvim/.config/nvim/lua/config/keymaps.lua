8a17487d50490094c0bda88f578edea61f1bc793
janis-h <janis_h.aguilar@yahoo.com>
2026-06-13 00:38:45 -0700

    nvim: organize keymaps

--- modules/nvim/.config/nvim/lua/config/keymaps.lua
+++ modules/nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
+-- General keymaps not owned by plugins
 local function opts(desc)
     return {
         noremap = true,
@@ -5,21 +6,41 @@ local function opts(desc)
     }
 end
 
+-- File actions
 vim.keymap.set('n', '<leader><leader>', ':w<CR>', opts('Write to file'))
 vim.keymap.set('n', '<leader><CR>', ':so ~/.config/nvim/init.lua<CR>', opts('Source init.lua'))
+
+-- File explorer
+vim.keymap.set('n', '<leader>e', ':Ex<CR>', opts('open Explorer'))
 vim.keymap.set('n', '<leader>v', ':Vex<CR>', opts('Vertical explorer'))
 vim.keymap.set('n', '<leader>s', ':Sex<CR>', opts('Horizontal explorer'))
-vim.keymap.set("n", "<leader>co", "<cmd>copen<cr>", opts("Open quickfix"))
-vim.keymap.set("n", "<leader>cn", "<cmd>cnext<cr>", opts("Next quickfix item"))
-vim.keymap.set("n", "<leader>cp", "<cmd>cprev<cr>", opts("Previous quickfix item"))
-vim.keymap.set("n", "<leader>cq", "<cmd>cexpr []<cr><cmd>cclose<cr>", opts("Clear quickfix"))
+
+-- Quickfix
+vim.keymap.set('n', '<leader>co', '<cmd>copen<cr>', opts('Open quickfix'))
+vim.keymap.set('n', '<leader>cn', "<cmd>cnext<cr>", opts('Next quickfix item'))
+vim.keymap.set('n', '<leader>cp', "<cmd>cprev<cr>", opts('Previous quickfix item'))
+vim.keymap.set('n', '<leader>cq', '<cmd>cexpr []<cr><cmd>cclose<cr>', opts('Clear quickfix'))
+
+-- Search
 vim.keymap.set('n', '<leader>cl', ':nohls<CR>', opts('Clear highlighted search'))
+
+-- Yank behavior
+vim.keymap.set('n', 'Y', 'y$', opts('Yank to end of line'))
+
+-- Delete / paste behavior
 vim.keymap.set("v", "<leader>d", '"_d', opts("Delete selection without yanking it"))
 vim.keymap.set('v', '<leader>p', '"_dp', opts('Delete current selection, yank into void register (to avoid replacing prev yank)'))
-vim.keymap.set('n', '<leader>P', 'o<ESC>Pk<CR>', opts('Create new line and paste copied text'))
-vim.keymap.set('n', '<leader>y', '"+y', opts('Yank into system clipboard'))
+vim.
+
+-- System clipboard
+vim.keymap.set({ 'n', 'v'}, '<leader>y', '"+y', opts('Yank into system clipboard'))
+vim.keymap.set('n', '<leader>P', '"+p', opts('Past from system keyboard'))
+
+-- Entire file actions
 vim.keymap.set('n', '<leader>Y', 'gg"+yG', opts('Yank entire file into system clipboard'))
-vim.keymap.set('v', '<leader>y', '"+y', opts('Copy into system keyboard'))
+vim.keymap.set('n', '<leader>D', 'gg"_dG', opts('Delete entire file without yanking it'))
+
+-- Move selected lines
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts('Move line up'))
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts('Move line down'))
 
