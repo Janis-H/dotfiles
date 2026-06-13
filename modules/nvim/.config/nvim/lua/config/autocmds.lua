35a597c232dd611eabed85dfd67d496061b2c615
janis-h <janis_h.aguilar@yahoo.com>
2026-06-13 01:54:38 -0700

    nvim: add tmux and grapple autocmdds

--- modules/nvim/.config/nvim/lua/config/autocmds.lua
+++ modules/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,27 @@
+-- Disable tmux navigation keys inside lazygit terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function(event)
+    local name = vim.api.nvim_buf_get_name(event.buf)
+
+    if name:match("lazygit") then
+      vim.keymap.set("t", "<C-j>", "<C-j>", { buffer = event.buf, silent = true })
+      vim.keymap.set("t", "<C-k>", "<C-k>", { buffer = event.buf, silent = true })
+    end
+  end,
+})
+
+-- Grapple tag list bugger mappings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "grapple",
+  callback = function(event)
+    local opts = { buffer = event.buf, silent = true }
+
+    vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", opts)
+    vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", opts)
+  end,
+})
+
 -- Trim trailing whitespace before saving most files.
 vim.api.nvim_create_autocmd("BufWritePre", {
     desc = "Trim trailing whitespace on save",
