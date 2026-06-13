1928afe61a96ce8a58d96e9303222b9f499c0bdf
janis-h <janis_h.aguilar@yahoo.com>
2026-06-12 23:40:31 -0700

    nvim: remove vim-plug usage from init.lua and plugin remaps

--- modules/nvim/.config/nvim/init.lua
+++ modules/nvim/.config/nvim/init.lua
@@ -13,64 +13,6 @@ require('config.options')
 vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
 
--- Plugins:
-local vim = vim
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin')
-
--- provides immediate navigation to important files
-Plug('cbochs/grapple.nvim') 
-
--- tmux navigator
-Plug('christoomey/vim-tmux-navigator')
-
--- fuzzy finder
-Plug('junegunn/fzf', { ['do'] = function () 
-    vim.fn['fzf#install']()
-end })
-
--- fuzzy finder for vim
-Plug('junegunn/fzf.vim') 
-
--- colorscheme
-Plug('ellisonleao/gruvbox.nvim') 
-
--- completion engine for neovim
-Plug('hrsh7th/nvim-cmp') 
-
--- lazygit for nvim
-Plug("kdheepak/lazygit.nvim")
-
--- autocomplete
-Plug('neoclide/coc.nvim', { ['branch'] = 'release'} )
-
--- lua functions that many plugins use
-Plug('nvim-lua/plenary.nvim') 
-
--- fuzzy finder over lists
-Plug('nvim-telescope/telescope.nvim') 
-
--- improved sorting for telescope
-Plug("nvim-telescope/telescope-fzf-native.nvim", {
-  ["do"] = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install"
-})
-
--- icons
-Plug('nvim-tree/nvim-web-devicons')
-
--- comment stuff out with gcc
-Plug('tpope/vim-commentary') 
-
--- git commands with :Git or :G
-Plug('tpope/vim-fugitive') 
-
--- mappings for easy deleting, changing and adding surrounding pairs
-Plug('tpope/vim-surround') 
-
--- end Plug plugin declaration
-vim.call('plug#end')
-
 -- Color Scheme: 
 vim.cmd.colorscheme("gruvbox")
 
@@ -82,10 +24,6 @@ local function opts(desc)
         desc = desc,
     }
 end
-
--- Plugin setup:
-require("plugins.grapple")
-require("plugins.telescope")
 
 -- Basic Remaps:
 vim.keymap.set('n', '<leader><leader>', ':w<CR>', opts('Write to file'))
@@ -105,23 +43,6 @@ vim.keymap.set('n', '<leader>Y', 'gg"+yG', opts('Yank entire file into system cl
 vim.keymap.set('v', '<leader>y', '"+y', opts('Copy into system keyboard'))
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts('Move line up'))
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts('Move line down'))
-
--- Plugin: Telescope remaps
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', opts('Find files'))
-vim.keymap.set('n', '<leader>gg', ':Telescope git_files<CR>', opts('Find git files'))
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', opts('Search Text'))
-vim.keymap.set('n', '<leader>fo', ':Telescope oldfiles<CR>', opts('Find recent files'))
-vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', opts('Find buffers'))
-vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', opts('Find help tags'))
-
--- Plugin: Fugitive remaps
-vim.keymap.set('n', '<leader>g:', ':G' , opts('Fugitive Git command'))
-vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", opts("Git status"))
-vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", opts("Git blame"))
-vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", opts("Git diff"))
-
--- Plugin: Lazygit remaps
-vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', opts('Open lazygit'))
 
 -- tmux-sessionizer
 -- Ctrl-f opens the project picker
