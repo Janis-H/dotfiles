31d31f623ef07b6c0b903cd805d206e630e01562
janis-h <janis_h.aguilar@yahoo.com>
2026-06-13 22:48:47 -0700

    nvim: update auto-session

--- modules/nvim/.config/nvim/lua/plugins/auto-session.lua
+++ modules/nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,17 +1,43 @@
 -- Restore Neovim sessions automatically
--- TODO: setup keymaps
 return {
-  "rmagatti/auto-session",
-  lazy = false,
+    "rmagatti/auto-session",
 
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-    session_lens = {
-        picker = "telescope",
+    lazy = false,
+
+    keys = {
+        { "<leader>ss", "<cmd>AutoSession save<cr>", desc = "Save session" },
+        { "<leader>sr", "<cmd>AutoSession restore<cr>", desc = "Restore session" },
+        { "<leader>sf", "<cmd>AutoSession search<cr>", desc = "Find session" },
+        { "<leader>sd", "<cmd>AutoSession deletePicker<cr>", desc = "Delete session" },
+        { "<leader>st", "<cmd>AutoSession toggle<cr>", desc = "Toggle session autosave" },
     },
-    -- log_level = 'debug',
-  },
+
+    opts = {
+        suppressed_dirs = {
+            "~/",
+            "~/Projects",
+            "~/Downloads",
+            "/"
+        },
+        session_lens = {
+            picker = "telescope",
+        },
+
+        -- Keep sessions scoped by project directory.
+        auto_save = true,
+        auto_restore = true,
+        auto_create = true,
+
+        -- Optional: uncomment if you want seperate sessions per Git branch
+        git_use_branch_name = true,
+
+        -- log_level = 'debug',
+    },
+
+    config = function(_, opts)
+        -- Recommended by auto-session for better restore behavior.
+        vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+        require("auto-session").setup(opts)
+    end,
 }
