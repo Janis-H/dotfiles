d3f0d1c79c0c23e7c9b0ae4efeb9382836f5da24
janis-h <janis_h.aguilar@yahoo.com>
2026-06-19 08:29:55 -0700

    nvim: condense lspconfig.lua and mason.lua into lsp.lua file

---
+++ modules/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,114 @@
+-- Installs, configures, and enables language servers
+local server_names = {
+    "lua_ls",
+    "ts_ls",
+    "eslint",
+    "html",
+    "cssls",
+    "jsonls",
+    "yamlls",
+    "bashls",
+    "pyright",
+    "ruff",
+    "marksman",
+    "sqlls",
+    "dockerls",
+    "docker_compose_language_service",
+}
+
+local function get_server_configs()
+    local schemastore = require("schemastore")
+
+    return {
+        lua_ls = {
+            settings = {
+                Lua = {
+                    diagnositics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        },
+        jsonls = {
+            settings = {
+                json = {
+                    schemas = schemastore.json.schemas(),
+                    validate = { enable = true },
+                },
+            },
+        },
+        yamlls = {
+            settings = {
+                yaml = {
+                    schmeStore = {
+                        enable = false,
+                        url = "",
+                    },
+                    schemas = schemastore.yaml.schemas(),
+                    validate = true,
+                    completion = true,
+                    hover = true,
+                },
+            },
+        },
+        pyright = {
+            settings = {
+                python = {
+                    analysis = {
+                        typeCheckingMode = "basic",
+                    },
+                },
+            },
+        },
+    }
+end
+
+return {
+    -- Install and manage external tools for NeoVim
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+    },
+
+    -- Auto-install LSP servers listed by nvim-lspconfig name
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            ensure_installed = server_names,
+            automatic_installation = true,
+            automatic_enable = false,
+        },
+    },
+
+    -- Configure and enable Neovim language servers.
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "mason-org/mason-lspconfig.nvim",
+            "b0o/schemastore.nvim",
+        },
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local server_configs = get_server_configs()
+
+            -- Register each server with optional config and shared completion capabilities
+            for _, server in ipairs(server_names) do
+                local server_config = server_configs[server] or {}
+
+                local config = vim.tbl_deep_extend('force', {}, server_config, {
+                    capabilities = capabilities,
+                })
+
+                vim.lsp.config(server, config)
+            end
+
+            -- Enable every server from the install list
+            vim.lsp.enable(server_names)
+        end,
+    }
+}
