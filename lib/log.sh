7d3680464db85584723fe2e2ee923c029efc56e8
janis-h <janis_h.aguilar@yahoo.com>
2026-06-19 06:21:04 -0700

    install: create title function

--- lib/log.sh
+++ lib/log.sh
@@ -8,6 +8,9 @@ RED='\033[0;31m'
 NO_COLOR='\033[0m'
 
 # TODO: create title function to allow for separation between sections
+title() {
+    echo -e "$GREEN \n\t--- $* ---\n"
+}
 
 info() {
     echo -e "${GREEN} INFO:${NO_COLOR} $*"
