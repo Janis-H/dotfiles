1562e4a302d37981bfa107af2813b1fae2289d0b
janis-h <janis_h.aguilar@yahoo.com>
2026-06-22 01:05:49 -0700

    zsh: source zsh-history-substring-search plugin

--- modules/zsh/.config/zsh/plugins.zsh
+++ modules/zsh/.config/zsh/plugins.zsh
@@ -15,3 +15,7 @@ source "$ZSH_PLUGINS_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 # syntax highlighting must be sourced last
 source "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+# cycle through matches of command history
+# zsh-history-substring must be sourced after syntax-highlighting
+source "$ZSH_PLUGINS_DIR/zsh-history-substring-search/zsh-history-substring-search.zsh"
