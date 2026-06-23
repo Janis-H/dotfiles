a7fe53a696d281bcc6d17566ab015b3ab40de123
janis-h <janis_h.aguilar@yahoo.com>
2026-06-23 15:01:03 -0700

    nvim: configure neotest

---
+++ modules/nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,52 @@
+-- currently disabled
+return {
+    {
+        "nvim-neotest/neotest",
+        -- enabled = false,
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+
+            -- treesitter setup
+            {
+                "nvim-treesitter/nvim-treesitter",
+                build = ":TSUpdate",
+                opts = {
+                    ensure_installed = {
+                        "javascript",
+                        "typescript",
+                        "tsx",
+                    },
+                },
+            },
+
+            -- adapters
+            "nvim-neotest/neotest-jest",
+        },
+        keys = {
+            { "<leader>nr", "<cmd>Neotest run<cr>" },
+            { "<leader>ni", "<cmd>Neotest output<cr>" },
+            { "<leader>ns", "<cmd>Neotest summary<cr>" },
+            { "<leader>na", "<cmd>lua require('neotest').run.run({ suite = true })<cr>" },
+        },
+        config = function()
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-jest")({
+                        jestCommand = "npm test --",
+                        jestArguments = function(defaultArguments, context)
+                            return defaultArguments
+                        end,
+                        jestConfigFile = "custom.jest.config.ts",
+                        env = { CI = true },
+                        cwd = function(path)
+                            return vim.fn.getcwd()
+                        end,
+                        isTestFile = require("neotest-jest.jest-util").defaultIsTestFile,
+                    }),
+                },
+            })
+        end,
+    },
+}
