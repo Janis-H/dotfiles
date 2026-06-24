cd6ea61b5c7634582ebe10baf8cdf0b6f7af9798
janis-h <janis_h.aguilar@yahoo.com>
2026-06-23 23:23:47 -0700

    nvim: italicize strings in gruvbox-material

--- modules/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ modules/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -30,6 +30,26 @@ return {
             vim.g.gruvbox_material_enable_italic = true
 
             vim.cmd.colorscheme('gruvbox-material')
+
+            -- Make strings italic like classic gruvbox
+            local function italicize(group)
+                local ok, hl = pcall(vim.api.nvim_get_hl, 0, {
+                    name = group,
+                    link = false,
+                })
+
+                if ok then
+                    hl.italic = true
+                    vim.api.nvim_set_hl(0, group, hl)
+                end
+            end
+
+            italicize("String")
+            italicize("Character")
+            italicize("@string")
+            italicize("@string.documentation")
+            italicize("@string.regexp")
+            italicize("@string.special")
         end,
     },
 }
