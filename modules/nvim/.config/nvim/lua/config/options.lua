72747925f9a454a2f10e021a8de4a89b0b11b37c
janis-h <janis_h.aguilar@yahoo.com>
2026-06-23 20:16:08 -0700

    nvim: update options

--- modules/nvim/.config/nvim/lua/config/options.lua
+++ modules/nvim/.config/nvim/lua/config/options.lua
@@ -10,7 +10,7 @@ vim.opt.relativenumber = true   -- show relative numbers around cursor
 
 -- wrapping and reading text
 vim.opt.wrap = true             -- long lines wrap
-vim.opt.linebreak = true        -- wrap at word bounderies
+vim.opt.linebreak = true        -- wrap at word boundaries
 vim.opt.breakindent = true      -- wrapped lines continue indented
 vim.opt.background = "dark"     -- colorschemes that can be made light or dark will be made dark
 vim.opt.termguicolors = true    -- enable true colors support
@@ -18,7 +18,6 @@ vim.opt.termguicolors = true    -- enable true colors support
 -- indentation
 vim.opt.autoindent = true       -- copy indent from current line
 vim.opt.smartindent = true      -- smart-indenting for C-like code
-vim.opt.smarttab = true         -- enters shiftwidth at line start
 vim.opt.expandtab = true        -- pressing <Tab> inserts spaces instead of tabs
 
 vim.opt.shiftwidth = 4          -- how many columns count as one level of indentation
@@ -30,7 +29,7 @@ vim.opt.guicursor = ""          -- cursor look in different modes
 vim.opt.cursorline = true       -- highlight current line
 vim.opt.scrolloff = 10          -- lines above/below cursor
 
--- signs, whitespace and visual helpers
+-- signs, whitespace, and visual helpers
 vim.opt.signcolumn = "yes"      -- show signs in number column (used by diagnostics, git signs and breakpoints)
 vim.opt.list = true             -- show whitespace characters
 vim.opt.listchars = {           -- customize how whitespace characters are shown
@@ -42,7 +41,7 @@ vim.opt.listchars = {           -- customize how whitespace characters are shown
 -- undo and history
 vim.opt.undolevels = 1000       -- number of undo levels
 vim.opt.undofile = true         -- persists undo history to disk
-vim.opt.undodir = os.getenv("HOME") .. '/.vim/undo'
+vim.opt.undodir = vim.fn.stdpath("state") .. '/undo'
 
 -- buffers and files
 vim.opt.autoread = true         -- auto-reload changed file
@@ -53,7 +52,7 @@ vim.opt.swapfile = false        -- create swapfile
 vim.opt.history = 1000                      -- number of commands kept in command-line history
 vim.opt.wildmenu = true                     -- enhances command-line completion
 vim.opt.wildmode = "longest:full,full"      -- controls how command-line completion behaves across repeated <TAB> presses
-vim.opt.showmode = false                    -- shows current mode in command area (often disabled if statusline shows the curretn mode)
+vim.opt.showmode = false                    -- shows current mode in command area (often disabled if statusline shows the current mode)
 
 -- configure how new splits should be opened
 vim.opt.splitright = true       -- vertical splits open to the right
@@ -65,6 +64,3 @@ vim.opt.updatetime = 250        -- CursorHold delay (ms)
 
 -- misc
 vim.opt.title = true            -- display window title in the terminal
-
-vim.cmd('syntax enable')
-vim.cmd('filetype plugin indent on')
