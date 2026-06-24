da92ff3883f88b692d3501e04136c845c2f51851
janis-h <janis_h.aguilar@yahoo.com>
2026-06-23 23:19:14 -0700

    nvim: add gruvbox-material theme

--- modules/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ modules/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,34 @@
 return {
     {
         'ellisonleao/gruvbox.nvim',
+        lazy = false,
         name = 'gruvbox',
 
-        -- move these into the table for whicher colorscheme is currently active
+        -- Uncomment lines below to set colorscheme
+        -- priority = 1000,
+        -- config = function()
+        --     vim.cmd.colorscheme('gruvbox')
+        -- end,
+    },
+    {
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        name = 'gruvbox-material',
+
+        -- Uncomment lines below to set colorscheme
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme('gruvbox')
+            ---@enum "hard" | "medium" | "soft"
+            ---@default "medium"
+            vim.g.gruvbox_material_background = "hard"
+
+            ---@enum "material" | "mix" | "original"
+            ---@default "material"
+            vim.g.gruvbox_material_foreground = "material"
+
+            vim.g.gruvbox_material_enable_italic = true
+
+            vim.cmd.colorscheme('gruvbox-material')
         end,
     },
 }
