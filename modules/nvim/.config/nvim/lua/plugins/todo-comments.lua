e41cda214521dd240ffb7563cc25a9cd74cccc5a
janis-h <janis_h.aguilar@yahoo.com>
2026-06-26 00:16:10 -0700

    nvim: add keymaps for todo-comments

--- modules/nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ modules/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -6,5 +6,9 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+  },
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" } },
+    { "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" } },
   }
 }
