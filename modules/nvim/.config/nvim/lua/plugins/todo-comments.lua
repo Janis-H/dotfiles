524f44fd6436859f51b348580ff31a98cae53888
janis-h <janis_h.aguilar@yahoo.com>
2026-06-26 00:18:02 -0700

    nvim: add TodoTelescope keymap

--- modules/nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ modules/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -10,5 +10,6 @@ return {
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" } },
     { "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" } },
+    { "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Previous todo comment" } },
   }
 }
