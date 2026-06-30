fb447c39b678d8035ce912c63149801739fb6de5
janis-h <janis_h.aguilar@yahoo.com>
2026-06-30 00:11:44 -0700

    nvim: disable render-markdown

--- modules/nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ modules/nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,6 +1,7 @@
 -- Render Markdown with better visual formatting
 return {
     "MeanderingProgrammer/render-markdown.nvim",
+    enabled = false,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons",
