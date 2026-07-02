78f9ec005e9d818ebf21c25c2ad63327e841d4d3
janis-h <janis_h.aguilar@yahoo.com>
2026-07-01 23:41:33 -0700

    nvim: fix todo-comments plugin highlighting

--- modules/nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ modules/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,11 +1,13 @@
 -- Highlight and search for todo comments like TODO, HACK, BUG in your code base
 return {
   "folke/todo-comments.nvim",
+  event = { "BufReadPost", "BufNewFile" },
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    signs = true,
+    highlight = {
+        comments_only = true,
+    },
   },
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" } },
