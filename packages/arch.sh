ac6cd8a961bcdf72386a5ec9d2c3c13b1aa73df9
janis-h <janis_h.aguilar@yahoo.com>
2026-07-02 16:11:27 -0700

    packages: remove dev_packages and add comments above core, server and desktop

--- packages/arch.sh
+++ packages/arch.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-# TODO: separate into core, server, dev_packages, and desktop packages
+# TODO: separate into core, server, and desktop packages
+
+# always install
 export CORE_PACKAGES=()
-export DEV_PACKAGES=()
+
+# server profile additions
 export SERVER_PACKAGES=()
+
+# desktop profile additions
 export DESKTOP_PACKAGES=()
 
 # TODO: add hyprland and/or sway
