6d09645f6e93828377fd05c5730047d112e958f1
janis-h <janis_h.aguilar@yahoo.com>
2026-07-05 23:44:38 -0700

    lib: update $OS reference in validate-os

--- lib/validate-os.sh
+++ lib/validate-os.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 validate_os() {
-    case "$os" in
+    case "$OS" in
         debian|arch|macos)
             return 0
             ;;
         *)
-            error "Unsupported OS: $os"
+            error "Unsupported OS: $OS"
             return 1
             ;;
     esac
