0537739da34f580c84797d481398e32e429d1777
janis-h <janis_h.aguilar@yahoo.com>
2026-07-07 22:38:24 -0700

    stow-modules: add default-modules function

---
+++ lib/build-default-modules-for-profile.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+build_default_modules_for_profile() {
+    local profile="${1:-core}"
+
+    DEFAULT_MODULES=()
+
+    case "$profile" in
+        core)
+            DEFAULT_MODULES+=(
+                "${CORE_STOW_MODULES[@]}"
+            )
+            ;;
+        server)
+            DEFAULT_MODULES+=(
+                "${CORE_STOW_MODULES[@]}"
+                "${DEV_BUILD_STOW_MODULES[@]}"
+            )
+            ;;
+        desktop)
+            DEFAULT_MODULES+=(
+                "${CORE_STOW_MODULES[@]}"
+                "${DEV_BUILD_STOW_MODULES[@]}"
+                "${DESKTOP_STOW_MODULES[@]}"
+            )
+            ;;
+        *)
+            echo "Unsupported profile: $profile" >&2
+            print_usage >&2
+            return 1
+            ;;
+    esac
+}
