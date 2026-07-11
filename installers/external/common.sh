91759a905c3c13d7286575570bc0ab754b735408
janis-h <janis_h.aguilar@yahoo.com>
2026-07-10 23:42:27 -0700

    installers: move install_zsh_plugins to external common file

--- installers/external/common.sh
+++ installers/external/common.sh
@@ -10,3 +10,34 @@ install_external_antidote() {
         "https://github.com/mattmc3/antidote.git" \
         "${ZDOTDIR:-$HOME}/.antidote"
 }
+
+# NOTE: Installs zsh plugins manually instead of using Antidote.
+#       It is kept here mainly for reference in case I ever switch back.
+#       To switch back, update .zshrc to source the plugins manually and add
+#       "zsh_plugins" to the relevant EXTERNAL_CORE arrays.
+#       The repos can coexist with Antidote, but .zshrc should only load one set.
+install_external_zsh_plugins() {
+    local zsh_plugins_dir="$HOME/.zsh/plugins"
+
+    run_cmd mkdir -p "$zsh_plugins_dir"
+
+    install_or_update_repo \
+        "https://github.com/zsh-users/zsh-autosuggestions" \
+        "$zsh_plugins_dir/zsh-autosuggestions"
+
+    install_or_update_repo \
+        "https://github.com/zsh-users/zsh-syntax-highlighting.git" \
+        "$zsh_plugins_dir/zsh-syntax-highlighting"
+
+    install_or_update_repo \
+        "https://github.com/Aloxaf/fzf-tab" \
+        "$zsh_plugins_dir/fzf-tab"
+
+    install_or_update_repo \
+        "https://github.com/zsh-users/zsh-completions" \
+        "$zsh_plugins_dir/zsh-completions"
+
+    install_or_update_repo \
+        "https://github.com/zsh-users/zsh-history-substring-search.git" \
+        "$zsh_plugins_dir/zsh-history-substring-search"
+}
