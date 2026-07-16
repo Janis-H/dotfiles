b357613a2d8638b8e527150942ec39aea1d536f8
janis-h <janis_h.aguilar@yahoo.com>
2026-07-16 13:35:39 -0700

    lib: update log colors (log.sh)

--- lib/log.sh
+++ lib/log.sh
@@ -2,36 +2,37 @@
 # Defines shared logging helpers for consistent info and error messages
 
 # --- Log helper functions ---
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-RED='\033[0;31m'
-NO_COLOR='\033[0m'
+CYAN='\033[1;36m'   # Sections
+GREEN='\033[0;32m'  # Info/success
+YELLOW='\033[1;33m' # Warnings
+RED='\033[0;31m'    # Errors
+RESET='\033[0m'     # Reset formatting
 
 section() {
     printf '\n%b\t=== %s ===%b\n\n' \
-        "$GREEN" \
+        "$CYAN" \
         "$*" \
-        "$NO_COLOR"
+        "$RESET"
 
 }
 
 info() {
     printf '%b INFO:%b %s\n' \
         "$GREEN" \
-        "$NO_COLOR" \
+        "$RESET" \
         "$*"
 }
 
 warn() {
     printf '%b WARN:%b %s\n' \
         "$YELLOW" \
-        "$NO_COLOR" \
+        "$RESET" \
         "$*"
 }
 
 error() {
     printf '%b WARN:%b %s\n' \
         "$RED" \
-        "$NO_COLOR" \
+        "$RESET" \
         "$*"
 }
