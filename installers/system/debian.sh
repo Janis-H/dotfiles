07805f0362bc1e82c44ba5b38d0b12b5f20ebe1f
janis-h <janis_h.aguilar@yahoo.com>
2026-07-16 10:21:34 -0700

    installers: update set_docker_repository function (debian)

--- installers/system/debian.sh
+++ installers/system/debian.sh
@@ -14,11 +14,24 @@ is_system_package_installed() {
 
 # --- Repository Setup ---
 setup_docker_repository() {
-    : # TODO: add docker steps
+    # Add Docker's official GPG key:
+    run_cmd sudo apt update
+    run_cmd sudo apt install ca-certificates curl
+    run_cmd sudo install -m 0755 -d /etc/apt/keyrings
+    run_cmd sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    run_cmd sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    # NOTE: This function's setup already exists
-    # in the `install_external_docker` function
-    # of the `installers/external/debian.sh` file
+    # Add the repository to Apt sources:
+    run_cmd sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+    run_cmd sudo apt update
 }
 
 setup_1password_repository() {
