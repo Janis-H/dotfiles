df22b396b16357b5d82532ba9cfc01e6e95c1d36
janis-h <janis_h.aguilar@yahoo.com>
2026-07-21 16:59:13 -0700

    installers: move install_external_yazi to common.sh

--- installers/external/common.sh
+++ installers/external/common.sh
@@ -60,3 +60,12 @@ install_external_rust_and_cargo() {
 
     curl https://sh.rustup.rs -sSf | sh
 }
+
+install_external_yazi() {
+    if ! command -v cargo; then
+        error "Cargo is required to install Yazi"
+        return 1
+    fi
+
+    run_cmd cargo install --force yazi-build
+}
--- installers/external/fedora.sh
+++ installers/external/fedora.sh
@@ -13,11 +13,3 @@ is_command_available(){
 }
 
 # --- External tool installers ---
-install_external_yazi() {
-    if ! command -v cargo; then
-        error "Cargo is required to install Yazi"
-        return 1
-    fi
-
-    run_cmd cargo install --force yazi-build
-}
