f1ffc96e31c9b6485f32e08d9ae21519d42e2510
janis-h <janis_h.aguilar@yahoo.com>
2026-07-21 18:46:58 -0700

    installers: add install_external_resvg (fedora)

--- installers/external/fedora.sh
+++ installers/external/fedora.sh
@@ -13,6 +13,21 @@ is_command_available(){
 }
 
 # --- External tool installers ---
+install_external_resvg() {
+    if is_command_available resvg; then
+        info "resvg already installed"
+        return 0
+    fi
+
+    if ! is_command_available cargo; then
+        info "Cargo is required to install resvg"
+        return 0
+    fi
+
+    info "Installing resvg"
+    run_cmd cargo install resvg --locked
+}
+
 install_external_lazydocker() {
     info "Installing or Updating lazydocker"
     run_cmd go install github.com/jesseduffield/lazydocker@latest
