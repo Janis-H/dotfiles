25a45e5060ea38a41a3296c1deeca3b3b6558a1f
janis-h <janis_h.aguilar@yahoo.com>
2026-07-28 23:09:00 -0700

    manifests: add 1password to desktop_packages (fedora)

--- manifests/fedora.sh
+++ manifests/fedora.sh
@@ -55,6 +55,7 @@ HEADLESS_PACKAGES=(
 # GUI/workstation additions:
 # - window managers, launchers, graphical terminals, editors, and GUI apps
 DESKTOP_PACKAGES=(
+    1password
     flatpak
     helium-bin
     ghostty
