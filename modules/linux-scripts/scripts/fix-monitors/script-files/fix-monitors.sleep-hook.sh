ec23dceab43362247da1adb10d6a73b1b3ac5413
janis-h <janis_h.aguilar@yahoo.com>
2026-07-30 11:08:40 -0700

    i3+linux-scripts: swap DisplayPort-1 and DisplayPort-0

--- modules/linux-scripts/scripts/fix-monitors/script-files/fix-monitors.sleep-hook.sh
+++ modules/linux-scripts/scripts/fix-monitors/script-files/fix-monitors.sleep-hook.sh
@@ -5,6 +5,6 @@ case "$1" in
     export DISPLAY=:0
     export XAUTHORITY="/home/residentgoose/Xauthority"
 
-    su residentgoose -c '/home/residentgoose/.local/bin/fix-monitors.sh'
+    su residentgoose -c '/home/residentgoose/.local/bin/fix-monitors'
     ;;
 esac
