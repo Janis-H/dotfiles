1d14b67f887f12d43d146fd5ce5bfce76d84d4cf
janis-h <janis_h.aguilar@yahoo.com>
2026-08-05 10:34:43 -0700

    installers: create install_external_nerd_font (common)

--- installers/external/common.sh
+++ installers/external/common.sh
@@ -5,6 +5,15 @@
 
 source "$DOTFILES_DIR/lib/install-or-update-repo.sh"
 
+# --- Helpers ---
+font_is_installed() {
+    local font_name="$1"
+
+    [[ -n "$(fc-list "$font_name" family)" ]]
+}
+
+# --- External functions ---
+
 # Manages zsh-plugins
 install_external_antidote() {
     install_or_update_repo \
@@ -53,6 +62,37 @@ install_external_autotiling() {
     info "Installing autotiling script (i3 and sway dependency)"
 
     run_cmd pip install autotiling
+}
+
+install_external_nerd_font() {
+    local font_name="JuliaMono"
+    local download_url="https://github.com/cormullion/juliamono/releases/latest/download/JuliaMono.zip"
+
+    local fonts_folder="$HOME/.local/share/fonts"
+
+    if font_is_installed "$font_name"; then
+        info "$font_name is already installed"
+        return 0
+    else
+        info "Installing $font_name font"
+    fi
+
+    run_cmd mkdir -p "$fonts_folder"
+
+    # Download and extract
+    run_cmd wget "$download_url"
+    run_cmd unzip "$font_name.zip" -d "$fonts_folder/$font_name"
+
+    # Rebuild font cache
+    run_cmd fc-cache -fv
+
+    # Verify Install
+    if font_is_installed "$font_name"; then
+        info "$font_name installed successfully"
+    else
+        error "Failed to install $font_name"
+        return 1
+    fi
 }
 
 install_external_herdr() {
