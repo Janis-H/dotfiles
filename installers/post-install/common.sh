1fa109b82f5f0c6a27d4f03dc96f602b0cfc5837
janis-h <janis_h.aguilar@yahoo.com>
2026-08-10 19:58:15 -0700

    installers: set gnome cursor-size to 28

--- installers/post-install/common.sh
+++ installers/post-install/common.sh
@@ -182,7 +182,7 @@ configure_gnome_theme() {
     run_cmd gsettings set "$schema" gtk-theme 'Nordic'
     run_cmd gsettings set "$schema" icon-theme 'Papirus-Dark'
     run_cmd gsettings set "$schema" cursor-theme 'Bibata-Modern-Ice'
-    run_cmd gsettings set "$schema" cursor-size 24
+    run_cmd gsettings set "$schema" cursor-size 28
     run_cmd gsettings set "$schema" font-name 'DejaVu Sans 12'
     run_cmd gsettings set "$schema" monospace-font-name 'FiraCode Nerd Font 13'
 }
