6fa2bcbb47e05b12ff81cad2d914137ded9d5253
janis-h <janis_h.aguilar@yahoo.com>
2026-08-19 15:08:02 -0700

    zsh: add herdr-sessionizer keybinding

--- modules/zsh/.config/zsh/keybindings.zsh
+++ modules/zsh/.config/zsh/keybindings.zsh
@@ -2,7 +2,7 @@
 # KEY-BINDINGS
 #############################################
 
-# enable vi-style ZLE keybindings
+# Enable vi-style ZLE keybindings
 bindkey -v
 
 # Edit the current command line in the configured editor
@@ -14,11 +14,14 @@ bindkey -M viins '^X^E' edit-command-line
 # tmux-sessionizer
 # Prefix + f opens the project picker.
 # Alt bindings run TS_SESSION_COMMANDS by index in the active project session.
-bindkey -s '^f' 'tmux-sessionizer\n' # <C-f>
+bindkey -s '^f' 'tmux-sessionizer\n'       # <C-f>
 bindkey -s '\e1' 'tmux-sessionizer -s 0\n' # Alt-1 or Option-1
 bindkey -s '\e2' 'tmux-sessionizer -s 1\n' # Alt-2 or Option-2
 bindkey -s '\e3' 'tmux-sessionizer -s 2\n' # Alt-3 or Option-3
 bindkey -s '\e4' 'tmux-sessionizer -s 3\n' # Alt-4 or Option-4
+
+# Alt-F opens the Herdr session picker.
+bindkey -s '\ef' 'herdr-sessionizer\n'
 
 # zsh-history-substring-search
 #
