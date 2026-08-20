f78189d3593862b81e539186f305cef0f0cd222f
janis-h <janis_h.aguilar@yahoo.com>
2026-08-19 17:09:28 -0700

    zsh: update ctrl+f session picker

--- modules/zsh/.config/zsh/keybindings.zsh
+++ modules/zsh/.config/zsh/keybindings.zsh
@@ -11,17 +11,14 @@ zle -N edit-command-line
 bindkey -M vicmd 'v' edit-command-line
 bindkey -M viins '^X^E' edit-command-line
 
-# tmux-sessionizer
-# Prefix + f opens the project picker.
+# Sessionizer
+# Ctrl-F opens the Herdr or tmux project picker for the current environment.
 # Alt bindings run TS_SESSION_COMMANDS by index in the active project session.
-bindkey -s '^f' 'tmux-sessionizer\n'       # <C-f>
+bindkey -s '^f' 'sessionizer\n'            # <C-f>
 bindkey -s '\e1' 'tmux-sessionizer -s 0\n' # Alt-1 or Option-1
 bindkey -s '\e2' 'tmux-sessionizer -s 1\n' # Alt-2 or Option-2
 bindkey -s '\e3' 'tmux-sessionizer -s 2\n' # Alt-3 or Option-3
 bindkey -s '\e4' 'tmux-sessionizer -s 3\n' # Alt-4 or Option-4
-
-# Alt-F opens the Herdr session picker.
-bindkey -s '\ef' 'herdr-sessionizer\n'
 
 # zsh-history-substring-search
 #
