0b0979a99ac13209c2d70e45555b8d2931e41458
janis-h <janis_h.aguilar@yahoo.com>
2026-08-21 16:12:52 -0700

    zsh: add grep color always

--- modules/zsh/.config/zsh/aliases.zsh
+++ modules/zsh/.config/zsh/aliases.zsh
@@ -13,3 +13,8 @@ fi
 # colorize --help pages with batcat
 alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
 alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
+# colorize grep results
+alias grep='grep --color=always'
+alias egrep='grep -E --color=always'
+alias fgrep='grep -f --color=always'
