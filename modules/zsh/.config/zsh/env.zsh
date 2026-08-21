46e76dbd2a4d0e0455bfb034f18c9b6f6a4e5387
janis-h <janis_h.aguilar@yahoo.com>
2026-08-21 12:21:17 -0700

    zsh: add bun completion

--- modules/zsh/.config/zsh/env.zsh
+++ modules/zsh/.config/zsh/env.zsh
@@ -34,11 +34,6 @@ else
     export VISUAL="nvim"
 fi
 
-# Node Version Manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-
 # Fzf Customization
 FZF_BASE="$HOME/.fzf"
 if command -v fd >/dev/null 2>&1; then
--- modules/zsh/.config/zsh/integrations.zsh
+++ modules/zsh/.config/zsh/integrations.zsh
@@ -7,3 +7,13 @@ fi
 if command -v zoxide >/dev/null 2>&1; then
     eval "$(zoxide init zsh)"
 fi
+
+# Bun shell integration
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+[ -s "$BUN_INSTALL/_bun" ] && source "$BUN_INSTALL/_bun"
+
+# Node Version Manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
